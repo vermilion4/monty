dfcbc3a0e63148c9e4436e559c09ce27421417ed
Adaeze Ndupu <andupu4@gmail.com>
2022-07-18 01:06:41 +0100

    Add queue

--- monty.h
+++ monty.h
@@ -61,6 +61,7 @@ char  *clean_line(char *content);
 void f_push(stack_t **head, unsigned int counter);
 void f_pall(stack_t **head, unsigned int number);
 void addnode(stack_t **head, int n);
+void addqueue(stack_t **head, int n);
 void f_pint(stack_t **head, unsigned int counter);
 int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
 void free_stack(stack_t *head);
@@ -68,5 +69,6 @@ void f_pop(stack_t **head, unsigned int counter);
 void f_swap(stack_t **head, unsigned int counter);
 void f_add(stack_t **head, unsigned int counter);
 void f_nop(stack_t **head, unsigned int counter);
+void f_queue(stack_t **head, unsigned int counter);
 
 #endif
